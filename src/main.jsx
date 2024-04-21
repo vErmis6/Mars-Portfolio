9eb28d9f2aeb9dc5586e80a55146db26ff020557
vErmis6 <pyorba6@gmail.com>
2024-04-21 10:49:53 -0700

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useRef, useEffect } from 'https://esm.sh/react@18.2.0'
+import ReactDOM from 'https://esm.sh/react-dom@18.2.0'
+import App from './App.jsx'
+import './index.css'
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
